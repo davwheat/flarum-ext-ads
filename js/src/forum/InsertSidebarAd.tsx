db4a631c27d427a32d58bf9dea38ef56db49ddae
David Wheatley <hi@davwheat.dev>
2021-08-07 00:12:04 +0200

    Only show sidebar ad on desktop and tablet

--- js/src/forum/InsertSidebarAd.tsx
+++ js/src/forum/InsertSidebarAd.tsx
@@ -9,12 +9,17 @@ import RefreshAds from './RefreshAds';
 import type ItemList from 'flarum/common/utils/ItemList';
 
 export default function InsertSidebarAd() {
+  const root = document.querySelector(':root') as HTMLHtmlElement;
+
   const AdCode = app.data['davwheat-ads.ad-code.sidebar'] as string;
 
   const Html = m.trust(AdCode) as ReturnType<Mithril.Static['trust']>;
 
   extend(IndexPage.prototype, 'sidebarItems', function (this: IndexPage, items: ItemList) {
-    items.add('davwheat-ads', <div class="davwheat-ad davwheat-ad-sidebar">{Html}</div>, -1000);
+    // Only show sidebar ad on desktop and tablet
+    if (['desktop-hd', 'desktop', 'tablet'].includes(getComputedStyle(root).getPropertyValue('--flarum-screen'))) {
+      items.add('davwheat-ads', <div class="davwheat-ad davwheat-ad-sidebar">{Html}</div>, -1000);
+    }
 
     return items;
   });
