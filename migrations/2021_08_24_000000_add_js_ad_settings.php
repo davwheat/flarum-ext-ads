6f592d9f512d753613c1daede19b01a8fa84e20e
David Wheatley <hi@davwheat.dev>
2021-08-25 00:32:00 +0100

    Add new migrations

---
+++ migrations/2021_08_24_000000_add_js_ad_settings.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of davwheat/ads.
+ *
+ * Copyright (c) 2021 David Wheatley.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Flarum\Database\Migration;
+
+return Migration::addSettings([
+    'davwheat-ads.ad-code.header.js' => '',
+    'davwheat-ads.ad-code.discussion_sidebar.js' => '',
+    'davwheat-ads.ad-code.between_posts.js' => '',
+    'davwheat-ads.ad-code.footer.js' => '',
+    'davwheat-ads.ad-code.discussion_header.js' => '',
+    'davwheat-ads.ad-code.sidebar.js' => '',
+]);
