7b1f7c7784c6de5db57416111ed9fdf87ae6d047
David Wheatley <hi@davwheat.dev>
2022-04-01 21:53:17 +0100

    chore: improve console warning text

--- js/src/forum/safelyEvalAdScript.ts
+++ js/src/forum/safelyEvalAdScript.ts
@@ -1,13 +1,19 @@
+/**
+ * Evals JS ad code without risking it bricking the forum frontend.
+ *
+ * **Please note that "safe" does not refer to preventing Javascript exploits or XSS attacks.**
+ *
+ * @param adPosition A name to show for the ad location
+ * @param script The Javascript code to be eval-ed
+ */
 export default function safelyEvalAdScript(adPosition: string, script: string): void {
   try {
     eval(script);
   } catch (e) {
     console.group('[davwheat/ads] Failed to execute ad script');
-    console.info(`An exception occurred while evaluating the ad script for ${adPosition}
+    console.info(`An exception occurred while evaluating the ad script for location "${adPosition}".
 
-To prevent your forum frontend breaking, we caught the error.
-
-Please be aware, however, that this may prevent your ads functioning in some cases.`);
+To prevent your forum frontend breaking, we caught the error. Please be aware, however, that this may prevent your ads functioning in some cases.`);
     console.info(
       `The stacktrace will be logged below to help you identify the issue. This may reference the ads extension, but the likelihood that the issue lies in there is extraordinarily miniscule.`
     );
