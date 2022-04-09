8fb24912af7a6923372359c615e58f4e12ddc9d0
David Wheatley <hi@davwheat.dev>
2022-04-09 03:03:32 +0100

    fix: add footer ad after first render

--- js/src/forum/InsertFooterAd.tsx
+++ js/src/forum/InsertFooterAd.tsx
@@ -4,30 +4,32 @@ import RefreshAds from './RefreshAds';
 import safelyEvalAdScript from './safelyEvalAdScript';
 
 export default function InsertFooterAd() {
-  if (areAdsBypassed()) return;
+  setTimeout(() => {
+    if (areAdsBypassed()) return;
 
-  const AdCode = app.data['davwheat-ads.ad-code.footer'] as string;
-  const Script = app.data['davwheat-ads.ad-code.footer.js'] as string;
+    const AdCode = app.data['davwheat-ads.ad-code.footer'] as string;
+    const Script = app.data['davwheat-ads.ad-code.footer.js'] as string;
 
-  const footer = document.createElement('footer');
-  footer.className = 'davwheat-ad davwheat-ad-footer';
-  footer.setAttribute('align', 'center');
+    const footer = document.createElement('footer');
+    footer.className = 'davwheat-ad davwheat-ad-footer';
+    footer.setAttribute('align', 'center');
 
-  document.querySelector('.App-content')!.append(footer);
-
-  footer.innerHTML = AdCode;
-
-  // Hacky way to detect URL change and re-create ad tag
-
-  const pushState = history.pushState;
-  history.pushState = function (...args) {
-    pushState.apply(history, args);
+    document.querySelector('.App-content')!.append(footer);
 
     footer.innerHTML = AdCode;
 
-    RefreshAds();
-    safelyEvalAdScript('footer', Script);
-  };
+    // Hacky way to detect URL change and re-create ad tag
 
-  RefreshAds();
+    const pushState = history.pushState;
+    history.pushState = function (...args) {
+      pushState.apply(history, args);
+
+      footer.innerHTML = AdCode;
+
+      RefreshAds();
+      safelyEvalAdScript('footer', Script);
+    };
+
+    RefreshAds();
+  }, 0);
 }
